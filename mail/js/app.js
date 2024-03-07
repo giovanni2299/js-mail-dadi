64d521e34308a49b5e02ea326c27fb47302b6b14
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-07 15:28:15 +0100

    ADD:email part of the document

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,8 +1,61 @@
-console.log('ciao mamma')
+// console.log('ciao mamma')
 
 // Mail
 // Chiedi all’utente la sua email,
-    //creo un imput mail in idex 
+    //creo un imput mail in idex e un bottone di invio
     //collego la imput al javascript 
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+//collego la imput email al javascript
+// const emailDOMElement = document.getElementById('email');
+
+const emailDOMElement = document.getElementById('email');
+
+// const emailElement = emailDOMElement.value;
+
+
+//ricavo il valore della mail e la visualizzo con value
+
+// const emailElement = emailDOMElement.value;
+
+//stampo la nuova constante che mi mostra la mail
+// console.log(emailElement);
+
+
+//collego il bottone all javascript
+const sendButtonDOMElement = document.getElementById('button')
+
+sendButtonDOMElement.addEventListener('click', function(){
+
+    const emailElement = emailDOMElement.value;
+    //creo un array per vedere se l' email inserita è gia registrata 
+
+    const checkEmailArray = ['giovanni-sorrentino@hotmail.it', 'Claudio-Rossi@gmail.com','Batman&Robin@bestfriends.forever','Non-sopporto@javascript.com']
+
+    //controllo che la mail sia fra quelle inserite nel arrey
+
+    let checkEmail = false 
+
+    for(let i = 0; i < checkEmailArray.length; i++){
+
+        const emailList = checkEmailArray[i];
+        
+        if(emailElement === emailList){
+            checkEmail = true;
+        }
+        // console.log(emailElement);
+    }
+    
+
+    if(checkEmail === true){
+        console.log('email presente')
+    } else{
+        console.log('email assente')
+    }
+})
+
+
+
