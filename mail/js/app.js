6ac58312aa64bd3a5ef34ff19e20905d9693e294
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-07 14:00:57 +0100

    ADD:created the document and started whit mail

---
+++ mail/js/app.js
@@ -0,0 +1,8 @@
+console.log('ciao mamma')
+
+// Mail
+// Chiedi all’utente la sua email,
+    //creo un imput mail in idex 
+    //collego la imput al javascript 
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
