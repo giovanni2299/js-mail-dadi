091a18fa6693116c79a46cde4263ac121d50ba23
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-07 16:19:23 +0100

    ADD: dice part

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -4,3 +4,31 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+//creo una variabile per generare un numero random per l'utente
+const userShot = document.getElementById('diceuser')
+userShot.addEventListener('click' ,function(){
+
+    const userNumber = []
+    const numberLaptop = []
+    
+    const randomNumbers = Math.floor(Math.random() * 6)+1;
+    userNumber.push(randomNumbers)
+    
+    const randomNumbersLaptop = Math.floor(Math.random() * 6)+1;
+    numberLaptop.push(randomNumbersLaptop)
+
+    console.log('number user: ', + userNumber , 'number Laptop: ' ,+ randomNumbersLaptop );
+
+
+    if(userNumber > numberLaptop){
+        console.log('YOU WIN! 🥳')
+    } else if(userNumber < numberLaptop ){
+        console.log('YOU LOSE ! 🥲')
+    } else{
+        console.log('TIE ! 🤝')
+    }
+
+})
+//creo una variabile per generare un numero random per il computer
+
